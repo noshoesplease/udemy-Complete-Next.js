a948d57661fa4fa78c4e964c4307dd58e83ad634
Devin Custodio <devin.roy.custodio@gmail.com>
2024-02-03 10:27:23 -0500

    added basic homepage

--- src/app/blogs/page.js
+++ src/app/blogs/page.js
@@ -1,8 +1,7 @@
 export default function Blogs() {
-    return (
-      <div>
-        <h1>Blogs</h1>
-      </div>
-    );
-  }
-  
+  return (
+    <div>
+      <h1>Blogs</h1>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,45 @@
+import Image from "next/image";
+import blogs from "@/content/blogs.json";
+import portfolios from "@/content/portfolios.json";
 
 export default function Home() {
   return (
-    <div>
-     hello world
-    </div>
+    <>
+      <>
+        <h2>Blogs</h2>
+        <ul>
+          {blogs.map((blog) => (
+            <li key={blog.id}>
+              <Image
+                src={blog.coverImage}
+                alt={blog.title}
+                width={100}
+                height={100}
+              />
+              {blog.title}
+              {blog.description}
+            </li>
+          ))}
+        </ul>
+      </>
+
+      <>
+        <h2>Portfolio</h2>
+        <ul>
+          {portfolios.map((portfolio) => (
+            <li key={portfolio.slug}>
+              <Image
+                src={portfolio.coverImage}
+                alt={portfolio.title}
+                width={100}
+                height={100}
+              />
+              {portfolio.title}
+              {portfolio.description}
+            </li>
+          ))}
+        </ul>
+      </>
+    </>
   );
 }
