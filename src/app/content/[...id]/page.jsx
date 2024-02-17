1a2f9ce2e2c7bf15ea4855ed1234e851071d754c
Devin Custodio <devin.roy.custodio@gmail.com>
2024-02-16 20:07:12 -0500

    added Catch all dynamic segment route

---
+++ src/app/content/[...id]/page.jsx
@@ -0,0 +1,26 @@
+const ContentPage = ({ params }) => {
+  /**
+   * This is a catch all route that uses Dynamic Segments.
+   * Dynamic Segments can be extended to catch-all subsequent
+   * segments by adding an ellipsis inside the brackets [...segmentName].
+   * For example, pages/shop/[...slug].js will match
+   * /shop/clothes, but also /shop/clothes/tops,
+   * /shop/clothes/tops/t-shirts, and so on.
+   */
+  return (
+    <div>
+      <p>
+        This is a catch all route that uses Dynamic Segments. Dynamic Segments
+        can be extended to catch-all subsequent segments by adding an ellipsis
+        inside the brackets [...segmentName]. For example,
+        pages/shop/[...slug].js will match /shop/clothes, but also
+        /shop/clothes/tops, /shop/clothes/tops/t-shirts, and so on.
+      </p>
+      <p>
+        The routes params are: {JSON.stringify(params.id)}
+      </p>
+    </div>
+  );
+};
+
+export default ContentPage;
