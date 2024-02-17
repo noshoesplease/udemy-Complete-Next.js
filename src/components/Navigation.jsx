b8e618d62a04b5a8bb61e5aa6ff056b43d44c92e
Devin Custodio <devin.roy.custodio@gmail.com>
2024-02-17 11:49:37 -0500

    refactored nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -14,48 +14,56 @@ function generateRandomString(length = 10) {
 export function Navigation() {
   const pathName = usePathname();
   const router = useRouter();
+
+  const navItems = [
+    {
+      isActive: pathName === "/",
+      href: "/",
+      text: "Home",
+    },
+    {
+      isActive: pathName.includes("/blogs"),
+      href: "/blogs",
+      text: "Blogs",
+    },
+    {
+      isActive: pathName.includes("/portfolios"),
+      href: "/portfolios",
+      text: "Portfolios",
+    },
+    {
+      isActive: pathName.includes("/todos"),
+      href: "/todos",
+      text: "Todos",
+    },
+    {
+      isActive: pathName.includes("/content"),
+      href: `/content/${generateRandomString()}`,
+      text: "Content",
+    },
+    {
+      isActive: pathName.includes("/books"),
+      href: `/books`,
+      text: "Books",
+    },
+    {
+      isActive: pathName.includes("/magazines"),
+      href: "/magazines",
+      text: "Magazines",
+    },
+  ];
+
   const render = (
     <>
       <ul className="navigation-list">
-        <li className="navigation-item">
-          <Link className={`link${pathName === "/" ? " active" : ""}`} href="/">
-            Home
+        {navItems.map((item, index) => 
+        <li key={index} className="navigation-item">
+          <Link className={`link${item.isActive ? " active" : ""}`} href={item.href}>
+            {item.text}
           </Link>
         </li>
-        <li className="navigation-item">
-          <Link
-            className={`link${pathName.includes("/blogs") ? " active" : ""}`}
-            href="/blogs"
-          >
-            Blogs
-          </Link>
-        </li>
-        <li className="navigation-item">
-          <Link
-            className={`link${
-              pathName.includes("/portfolios") ? " active" : ""
-            }`}
-            href="/portfolios"
-          >
-            Portfolios
-          </Link>
-        </li>
-        <li className="navigation-item">
-          <Link
-            className={`link${pathName.includes("/todos") ? " active" : ""}`}
-            href="/todos"
-          >
-            Todos
-          </Link>
-        </li>
-        <li className="navigation-item">
-          <Link
-            className={`link${pathName.includes("/content") ? " active" : ""}`}
-            href={`/content/${generateRandomString()}`}
-          >
-            Content
-          </Link>
-        </li>
+        )}
+     
         <li className="navigation-item">
           <button
             className="btn btn-primary"
