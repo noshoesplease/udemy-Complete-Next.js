e29c77e60ad038ce64a2f2afeb9373a69113fe9a
Devin Custodio <devin.roy.custodio@gmail.com>
2024-02-16 22:21:00 -0500

    added loading page; added suspensful fetching

---
+++ src/app/loading.jsx
@@ -0,0 +1,9 @@
+
+
+const  LoadingPage = () => {
+  return (
+    <div> Loading Content</div>
+  )
+}
+
+export default  LoadingPage
---
+++ src/app/todos/[id]/page.jsx
@@ -0,0 +1,45 @@
+import { Suspense } from "react";
+
+const getTodos = async (todoID) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const reponse = await fetch(
+    `https://jsonplaceholder.typicode.com/todos/${todoID}`
+  );
+  if (!reponse.ok) {
+    // throw new Error('Network response was not ok');
+    return false;
+  }
+
+  return reponse.json();
+};
+
+const Todo = async ({ id }) => {
+  const todo = await getTodos(id);
+  const render = (
+    <div>
+      <h1>{todo.id}</h1>
+      <div>title: {todo.title}</div>
+    </div>
+  );
+  if (!todo) {
+    return <div>Todo not found</div>;
+  }
+  return render;
+};
+
+const ToDoDetailPage = async ({ params }) => {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+
+  const render = (
+    <div>
+      Todo:
+      <Suspense fallback={<div>Loading...</div>}>
+        <Todo id={params.id} />
+      </Suspense>
+    </div>
+  );
+  return render;
+};
+
+export default ToDoDetailPage;
